c7c332ed9c95592692bd30cd4c7c32eb1e1d5ad8
Piedra Ridwan Azra Pulungan <azrapulungan612@gmail.com>
2026-09-02 14:39:04 +0700

    feat: configure allowed hosts and whitenoise for PWS deployment

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-k0r_i)(iiz22gh*cp92qv40$y*o*fvk!6jr6vm7433-o(e+m02
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "<URL deployment PWS kamu>"]
 
 
 # Application definition
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +144,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
